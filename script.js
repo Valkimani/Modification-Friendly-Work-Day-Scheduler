4c998cd3a15a1767f27bda1fbd926eb538c6487c
Valkimani <66477953+Valkimani@users.noreply.github.com>
2020-10-04 17:29:43 -0400

    Created DOM and JS variables.

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-// Added  current day to show full date in the title
+// Added current day to show full date in the title
 
 $("#currentDay").text(moment().format("dddd MMMM Do YYYY, h:mm a"));
+
+// DOM variables
+var hour = $(".hour");
+var timeBlock = $(".time-block");
+var currentDay = $(".currentDay");
+var past = $(".past");
+var present = $(".present");
+var future = $(".future");
+var saveBtn = $(".saveBtn");
+// JS variables
+var time = moment().format("dddd, MMM Do, YYY");
+var currentHour = moment().moment.format("H");
+var businessHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];

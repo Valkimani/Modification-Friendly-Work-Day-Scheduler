50cca38278ba193128930825c772f901a7c3526c
Valkimani <66477953+Valkimani@users.noreply.github.com>
2020-10-06 20:30:38 -0400

    Created a for loop to include the 3 steps. Update the html file.

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 // Added current day to show full date in the title
+$(document).ready(function () {
+  // Dom variables
+  var hour = $(".hour");
+  console.log(hour);
+  var timeBlockContainer = $(".time-block");
+  var militaryHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+  var businessHours = [
+    "9:00 AM",
+    "10:00 AM",
+    "11:00 AM",
+    "12.00 PM",
+    "1:00 PM",
+    "2:00 PM",
+    "3:00 PM",
+    "4:00 PM",
+    "5:00 PM",
+  ];
 
-$("#currentDay").text(moment().format("dddd MMMM Do YYYY, h:mm a"));
+  $("#currentDay").text(moment().format("dddd MMMM Do YYYY, h:mm a"));
 
-// DOM variables
-var hour = $(".hour");
-var timeBlock = $(".time-block");
-var currentDay = $(".currentDay");
-var past = $(".past");
-var present = $(".present");
-var future = $(".future");
-var saveBtn = $(".saveBtn");
-// JS variables
-var time = moment().format("dddd, MMM Do, YYY");
-var currentHour = moment().moment.format("H");
-var businessHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+  // Created a for loop and added 3 steps
+  for (var i = 0; i < businessHours.length; i++)
+    var timeDisplay = $("<div>")
+      .addClass("hour col-sm-1")
+      .text(businessHours[i]);
+
+  {
+  }
+});
